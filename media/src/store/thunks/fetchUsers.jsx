ddf6ab862be14236dd78e4b2aaeb561bdda6689a
L C <LF9904@gmail.com>
2025-07-24 22:39:37 -0400

    added a thunk

---
+++ media/src/store/thunks/fetchUsers.jsx
@@ -0,0 +1,10 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const res = await axios.get("http://localhost:3005/users");
+
+  return res.data;
+});
+
+export {fetchUsers};
