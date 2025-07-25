7e908cf4eaeec8e84cb860d3b5875666430bc78b
L C <LF9904@gmail.com>
2025-07-24 20:52:46 -0400

    created the redux store

--- media/src/index.jsx
+++ media/src/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import {store} from "./store";
 import App from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App></App>);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
