fe9b2af82c8deb0e2ac2530e84705ed57d5d773a
L C <LF9904@gmail.com>
2025-07-26 19:10:05 -0400

    added a pause for testing

--- media/src/store/thunks/fetchUsers.jsx
+++ media/src/store/thunks/fetchUsers.jsx
@@ -4,11 +4,21 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const res = await axios.get("http://localhost:3005/users");
 
+  //  DEV ONLY!!!
+  await pause(9000);
+
   return res.data;
 });
 
 // fetchUsers.pending === 'users/fetch/pending'
 // fetchUsers.fulfilled === 'users/fetch/fulfilled'
 // fetchUsers.rejected === 'users/fetch/rejected'
+
+//  DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export {fetchUsers};
