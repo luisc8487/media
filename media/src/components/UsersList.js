6f7cf9f6c2f8622093f0fa615b28c57b1b3e5e00
L C <LF9904@gmail.com>
2025-07-29 19:10:09 -0400

    added proper setup with parcel

---
+++ media/src/components/UsersList.js
@@ -0,0 +1,25 @@
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchUsers} from "../store";
+import Skeleton from "./Skeleton";
+
+function UsersList() {
+  const dispatch = useDispatch();
+  const {isLoading, data, error} = useSelector((state) => {
+    return state.users; // {data: [], isLoading: false, error: null}
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+  return <div>{data.length}</div>;
+}
+export default UsersList;
---
+++ media/src/store/thunks/fetchUsers.js
@@ -0,0 +1,24 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const res = await axios.get("http://localhost:3005/users");
+
+  //  DEV ONLY!!!
+  await pause(10000);
+
+  return res.data;
+});
+
+// fetchUsers.pending === 'users/fetch/pending'
+// fetchUsers.fulfilled === 'users/fetch/fulfilled'
+// fetchUsers.rejected === 'users/fetch/rejected'
+
+//  DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+export {fetchUsers};
