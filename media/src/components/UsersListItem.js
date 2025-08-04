d69314d3a2b148f8fa1ef3a36e5b61625a37aac6
L C <LF9904@gmail.com>
2025-08-03 22:39:19 -0400

    fixed a delete error

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -6,18 +6,20 @@ import {useThunk} from "../hooks/use-thunks";
 function UsersListItem({user}) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
-  const handleClick = () =>{
-    doRemoveUser(user)
-  }
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
 
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
-        <Button loading={isLoading} onClick={handleClick}>
-          <GoTrashcan />
-        </Button>
-        {error && <div>Error deleting user.</div>}
-        {user.name}
+        <div className="flex flex-row items-center justify-between">
+          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+            <GoTrashcan />
+          </Button>
+          {error && <div>Error deleting user.</div>}
+          {user.name}
+        </div>
       </div>
     </div>
   );
--- media/src/store/thunks/removeUser.js
+++ media/src/store/thunks/removeUser.js
@@ -2,10 +2,10 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
-  const res = await axios.delete(`http://localhost:3005/users/${user.id}`);
+  await axios.delete(`http://localhost:3005/users/${user.id}`);
 
   //FIX!!!
-  return res.data;
+  return user;
 });
 
 export {removeUser};
