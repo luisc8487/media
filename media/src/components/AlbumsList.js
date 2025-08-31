8b37696af649ad8477cfaec911085b65d65b916e
L C <LF9904@gmail.com>
2025-08-31 17:15:16 -0400

    rendered the list

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -1,11 +1,34 @@
 import {useFetchAlbumsQuery} from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({user}) {
   const {data, error, isLoading} = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading)
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
 
-  return <div> Albums for {user.name}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          Lists of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
