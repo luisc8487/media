37e247923c6170ce659b454a4730053c715233a1
L C <LF9904@gmail.com>
2025-09-01 21:06:31 -0400

    added a pause for testing

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -6,6 +6,7 @@ import Button from "./Button";
 function AlbumsList({user}) {
   const {data, error, isLoading} = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
+  console.log(results)
 
   const handleAddAlbum = () => {
     addAlbum(user);
@@ -13,7 +14,7 @@ function AlbumsList({user}) {
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
@@ -30,9 +31,9 @@ function AlbumsList({user}) {
 
   return (
     <div>
-      <div>
-        Albums for {user.name}
-        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Add Album</Button>
       </div>
       <div>{content}</div>
     </div>
