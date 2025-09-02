972461c955f3036cc92f86af0feb2c8edeef776e
L C <LF9904@gmail.com>
2025-09-01 21:45:08 -0400

    refactored the list

---
+++ media/src/components/AlbumsListItem.js
@@ -0,0 +1,18 @@
+import { GoTrashcan } from "react-icons/go";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+
+function AlbumsListItem({album}) {
+  const header = <div>
+    <Button><GoTrashcan/></Button>
+    {album.title}
+    </div>;
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      Lists of photos in the album
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
