9405777269968483b70da5c900e4bf691a1293c4
L C <LF9904@gmail.com>
2025-09-02 21:00:58 -0400

    added clever with cache tag

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -22,7 +22,7 @@ const albumsApi = createApi({
     return {
       removeAlbum: builder.mutation({
         invalidatesTags: (result, error, album) => {
-          return [{type: "Album", id: album.userId}];
+          return [{type: "Album", id: album.id}];
         },
         query: (album) => {
           return {
@@ -33,7 +33,7 @@ const albumsApi = createApi({
       }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{type: "Album", id: user.id}];
+          return [{type: "UserAlbums", id: user.id}];
         },
         query: (user) => {
           return {
@@ -48,7 +48,12 @@ const albumsApi = createApi({
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{type: "Album", id: user.id}];
+          console.log(result);
+          const tags = result.map( album => {
+            return {type: 'Album', id: album.id}
+          });
+          tags.push({type: 'UserAlbums', id: user.id});
+          return tags;
         },
         query: (user) => {
           return {
